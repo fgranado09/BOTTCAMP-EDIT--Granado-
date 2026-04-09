0c1088fd64917feb9c32a6e0987d41b1e1d02190
Florencia Granado <fgranado.rumar@gmail.com>
2026-04-08 22:54:11 -0300

    Repasando

--- modulos/mod-dos/modulo-dos.py
+++ modulos/mod-dos/modulo-dos.py
@@ -77,3 +77,56 @@ else:
 
 separador()
 
+# Entrada de Datos
+
+nombre = input("Ingrese su nombre: ") # El input siempre devuelve un string, por eso no es necesario poner comillas 
+cursos = input("Cuantos cursos realizaste?")
+
+print("Hola", nombre) # El input le asigna un valor a la variable nombre, que luego se puede usar en el programa
+print("Hiciste", cursos, "cursos") # El input le asigna un valor a la variable cursos, que luego se puede usar en el programa
+
+separador()
+
+# Mala practica
+
+# edad = input("Ingresa tu edad: ") # No permite hacer operaciones matematicas con esta variable, porque el input devuelve un string, por eso es necesario convertirlo a un entero
+
+# Buena Practica
+
+edad = int(input("Ingresa tu edad: ")) # Convertimos el string a un entero, para poder hacer operaciones matematicas con esta variable
+if edad >= 16: 
+    print("Puede votar")
+else:
+    print("No puede votar")
+
+
+separador()
+
+# Conversiones entre tipos de datos
+
+# Agregar prefijo a un numero de celular
+celular = "1124557307"
+celular_con_prefijo = "+54" + celular
+print(celular_con_prefijo)
+print(type(celular_con_prefijo))
+
+# Calcular el siguiente numero de celular
+celular_siguiente = int(celular) + 1
+print(celular_siguiente)       
+print(type(celular_siguiente))
+
+# Agregar prefijo al siguiente numero de celular
+celular_siguiente_con_prefijo = "+54" + str(celular_siguiente)
+print(celular_siguiente_con_prefijo)
+print(type(celular_siguiente_con_prefijo))
+
+separador() 
+
+# Bucle While
+
+a = 1
+while a < 5: # Mientras a sea menor o igual a 5, se va a ejecutar el bloque de codigo del while hasta que el booleano sea false.
+    print("Hola mundo")
+    print("Desde python")
+    a = a + 1 # Incrementamos el valor de a en 1, para evitar un bucle infinito
+print("Fin del programa")

57e56f60999fc254a1604a12fd2b0a5d533ba46b
fgranado09 <fgranado09@gmail.com>
2026-04-20 14:45:45 -0300

    Prueba

---
+++ Proyecto-Integrador/integrador-4.py
@@ -0,0 +1,17 @@
+db = [
+    {
+        "id" : 1,
+        "nombre" : "Florencia",
+        "apellido" : "Granado",
+        "cantidad_cursos" : 3
+    }
+]
+
+def mostar_menu():
+    print("Bienvenidos al sistema de gestion de alumnos")
+    print("1- Agregar Alumno")
+    print("2- Mostrar Alumnos")
+    print("3- Modificar Alumno")
+    print("4- Eliminar Alumno")
+    print("5- Salir")
+

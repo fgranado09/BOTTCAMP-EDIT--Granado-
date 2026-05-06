3f3107407d537d25c02b6d9437fedb4eb1a84131
Florencia Granado <fgranado.rumar@gmail.com>
2026-05-05 21:33:45 -0300

    Probando diferentes lenguajes

---
+++ modulos/mod-seis/HolaMundo.java
@@ -0,0 +1,5 @@
+public class HolaMundo { // Define la clase. En java, todo el código debe estar dentro de una clase.
+    public static void main(String[] args) { // Punto de entrada del programa. Por donde Java empieza a ejecutar el codigo.
+        System.out.println("¡Hola Mundo!"); // Imprime el mensaje en la consola.
+    }
+}
---
+++ modulos/mod-seis/HolaMundo/Program.cs
@@ -0,0 +1,9 @@
+﻿using System; // Importa las herramiejntas basicas de C#, como la consola.
+
+class Saludo {
+    static void Main(string[] args) { // Punto de entrada del programa.
+        Console.Write("¿Cuál es tu nombre? "); // Muestra texto sin saltar de linea, para que el usuario escriba.
+        string nombre = Console.ReadLine(); // Lee lo que el usuario escribe y lo guarda en la variable "nombre".
+        Console.WriteLine("¡Hola, " + nombre + "! Bienvenido."); // Muestra el saludo con salto de linea al final
+    }
+}
